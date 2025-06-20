09a93fd6f073a2c383d31b1729bc07aada7fafb3
Anton Zorkin <zanton1978@gmail.com>
2025-06-20 19:20:03 +0300

    commit message

--- js/task-1.js
+++ js/task-1.js
@@ -4,7 +4,7 @@ function isEnoughCapacity(products, containerSize) {
   for (let i = 0; i < arr.length; i++) {
     amount += arr[i];
   }
-  return amount < containerSize;
+  return amount <= containerSize;
 }
 console.log(isEnoughCapacity({ apples: 2, grapes: 3, carrots: 1 }, 8)); // true
 console.log(isEnoughCapacity({ apples: 4, grapes: 6, lime: 16 }, 12)); // false
